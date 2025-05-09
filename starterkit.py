0ef1d3870b1705674aa316c43b37a8f55f586379
Nivesara <65033649+Nivesara@users.noreply.github.com>
2025-05-10 00:50:08 +0100

    Add files via upload

---
+++ starterkit.py
@@ -0,0 +1,46 @@
+import sys, string
+import os
+import socket
+import time
+import operator
+import boto3
+import json
+from pyspark.sql import SparkSession
+from datetime import datetime
+from pyspark.sql.functions import from_unixtime, date_format
+from pyspark.sql.functions import to_date, count, col
+from graphframes import *
+
+
+
+if __name__ == "__main__":
+
+    spark = SparkSession\
+        .builder\
+        .appName("TestDataset")\
+        .getOrCreate()
+    
+    # shared read-only object bucket containing datasets
+    s3_data_repository_bucket = os.environ['DATA_REPOSITORY_BUCKET']
+
+    s3_endpoint_url = os.environ['S3_ENDPOINT_URL']+':'+os.environ['BUCKET_PORT']
+    s3_access_key_id = os.environ['AWS_ACCESS_KEY_ID']
+    s3_secret_access_key = os.environ['AWS_SECRET_ACCESS_KEY']
+    s3_bucket = os.environ['BUCKET_NAME']
+
+    hadoopConf = spark.sparkContext._jsc.hadoopConfiguration()
+    hadoopConf.set("fs.s3a.endpoint", s3_endpoint_url)
+    hadoopConf.set("fs.s3a.access.key", s3_access_key_id)
+    hadoopConf.set("fs.s3a.secret.key", s3_secret_access_key)
+    hadoopConf.set("fs.s3a.path.style.access", "true")
+    hadoopConf.set("fs.s3a.connection.ssl.enabled", "false")
+
+    rideshare_data = # /read from the bucket file
+    taxi_zone_lookup_df = # /read from the bucket file
+
+
+
+
+
+    
+    spark.stop()
